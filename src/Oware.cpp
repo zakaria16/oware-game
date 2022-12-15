e3e7bd89c5a31e58f1b5c7bec86ea907af4dc86b
Zakaria Mohammed <zakaria.m1662@gmail.com>
2022-12-15 19:49:13 +0000

    move Oware implementation to .cpp

--- src/Oware.cpp
+++ src/Oware.cpp
@@ -1,1 +1,42 @@
 #include "Oware.hpp"
+void oware::Oware::addSeedsAt(const oware::House &house, const int val) {
+    auto &seed = board[house.getX()][house.getY()];
+    seed += val;
+}
+std::vector<oware::House> oware::Oware::sow(oware::House house) {
+    auto curSeed = getSeedsAt(house);
+    if (curSeed<=0 || !house.isValid()) { return {}; }
+
+    setSeedsAt(house, 0);
+    std::vector<House> affectedHoles;
+    for (uint8_t n = 1; n <= curSeed; n++) {
+        house++;
+        addSeedsAt(house, 1);
+        affectedHoles.push_back(house);
+    }
+    return affectedHoles;
+}
+uint8_t oware::Oware::getSeedsAt(const oware::House &house) {
+    return board[house.getX()][house.getY()];
+}
+void oware::Oware::setSeedsAt(const oware::House &house, const uint8_t val) {
+    board[house.getX()][house.getY()] = val;
+}
+void oware::Oware::printBoard() const {
+    std::cout << *this;
+}
+std::string oware::Oware::toString() const {
+    std::stringstream ss;
+    ss << std::endl << std::endl;
+    for (const auto &b: board) {
+        for (const uint8_t seeds: b) {
+            ss << " | " << (int) seeds;
+        }
+        ss << " |" << std::endl;
+        ss << " -------------------------\n";
+    }
+    return ss.str();
+}
+oware::Oware::BoardType oware::Oware::getBoard() {
+    return board;
+}
--- src/Oware.hpp
+++ src/Oware.hpp
@@ -13,63 +13,31 @@
 namespace oware {
 
     class Oware {
+        using House = oware::House;
+
     public:
         using BoardType = std::array<std::array<uint8_t, 6>, 2>;
 
-        void addSeedsAt(const House &house, const int val) {
-            auto &seed = board[house.getX()][house.getY()];
-            seed += val;
-        }
+        void addSeedsAt(const House &house, int val);
 
         /// select the seed in a given house and sow them one by anti-clockwise till seeds finish
         /// \param house the house to select from and start moving
         /// \return  the vector of holes affected during the sow
-        std::vector<House> sow(House house) {
-            auto curSeed = getSeedsAt(house);
-            if (curSeed<=0 || !house.isValid()) { return {}; }
-
-            setSeedsAt(house, 0);
-            std::vector<House> affectedHoles;
-            for (uint8_t n = 1; n <= curSeed; n++) {
-                house++;
-                addSeedsAt(house, 1);
-                affectedHoles.push_back(house);
-            }
-            return affectedHoles;
-        }
+        std::vector<House> sow(House house);
 
         /// Get the seeds at given house
         /// \param house
         /// \return
-        uint8_t getSeedsAt(const House &house) {
-            return board[house.getX()][house.getY()];
-        }
+        uint8_t getSeedsAt(const House &house);
 
-        void setSeedsAt(const House &house, const uint8_t val) {
-            board[house.getX()][house.getY()] = val;
-        }
+        void setSeedsAt(const House &house, uint8_t val);
 
-        void printBoard() const {
-            std::cout << *this;
-        }
+        void printBoard() const;
 
 
-        [[nodiscard]] std::string toString() const {
-            std::stringstream ss;
-            ss << std::endl << std::endl;
-            for (const auto &b: board) {
-                for (const uint8_t seeds: b) {
-                    ss << " | " << (int) seeds;
-                }
-                ss << " |" << std::endl;
-                ss << " -------------------------\n";
-            }
-            return ss.str();
-        }
+        [[nodiscard]] std::string toString() const;
 
-        BoardType getBoard() {
-            return board;
-        }
+        BoardType getBoard();
 
         friend std::ostream &operator<<(std::ostream &os, const Oware &oware) {
             os << oware.toString();
